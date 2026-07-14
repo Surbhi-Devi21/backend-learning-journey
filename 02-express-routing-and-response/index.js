d7dd870841384dc2330c597940f9de86590fa8e0
Surbhi Devi <surbhimishra3135@gmail.com>
2026-07-14 11:23:01 +0530

    02-express-routing-and-response

---
+++ 02-express-routing-and-response/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req , res) => {
+  res.send("Hello World");
+})
+
+app.get("/about", (req , res) => {
+  res.send("About Page");
+})
+
+app.get("/Home" , (req , res) => {
+  res.send("Server is running successfully.  This is Home Page...   This project is built while learning Express.js.")
+})
+
+app.listen(3000);

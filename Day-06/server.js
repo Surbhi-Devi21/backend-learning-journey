b77b152293168dffd622139776e99b924c62ecd6
Surbhi Devi <surbhimishra3135@gmail.com>
2026-07-18 11:46:07 +0530

    remove mongo string url

--- Day-06/server.js
+++ Day-06/server.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 const mongoose = require('mongoose');
 
 function connectTodb() {
-  mongoose.connect("mongodb://surbhimishra3135_db_user:vnjbyN2og5D5sR33@ac-hjvb1mc-shard-00-00.fddgl2c.mongodb.net:27017,ac-hjvb1mc-shard-00-01.fddgl2c.mongodb.net:27017,ac-hjvb1mc-shard-00-02.fddgl2c.mongodb.net:27017/?ssl=true&replicaSet=atlas-idqndv-shard-0&authSource=admin&appName=Cluster0/Day06") 
+  mongoose.connect("mongodb://string") 
   .then(() => {
     console.log("connected to database");
   })
